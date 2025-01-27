fd35adb6ded69a9ee5369cbd8f66db0224969cbd
nikolaslyonsLW <166641030+nikolaslyonsLW@users.noreply.github.com>
2025-01-27 15:16:03 -0700

    Delete test_project_dev directory

--- test_project_dev/test_function.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def lambda_handler(event, context):
-    # TODO Implement Logic
-    return {{
-        'statusCode': 200,
-        'body': json.dumps('Create new lambda function')
-    }}
