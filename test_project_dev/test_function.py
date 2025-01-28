0b0156cf4d96e4c3fbf2fb2fbb77ead64655ea4b
Nikolas Lyons <nikolas.lyons@lambweston.com>
2025-01-28 08:37:28 -0700

    test commit

--- test_project_dev/test_function.py
+++ test_project_dev/test_function.py
@@ -2,6 +2,7 @@ import json
 
 def lambda_handler(event, context):
     # TODO Implement Logic
+    variable = "this is a test"
     return {{
         'statusCode': 200,
         'body': json.dumps('Create new lambda function')
