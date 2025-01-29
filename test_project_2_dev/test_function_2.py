a07cbc3b16ca789e116a24b5adce7889e42b0ad7
Nikolas Lyons <nikolas.lyons@lambweston.com>
2025-01-29 11:43:09 -0700

    checking to see a update

--- test_project_2_dev/test_function_2.py
+++ test_project_2_dev/test_function_2.py
@@ -2,7 +2,7 @@ import json
 
 def lambda_handler(event, context):
     # TODO Implement Logic
-    varible_2 = "this is another test to make sure its workking ohh noooo"
+    varible_2 = "this is another test to make sure its workking ohh"
     return {{
         'statusCode': 200,
         'body': json.dumps('Create new lambda function')
